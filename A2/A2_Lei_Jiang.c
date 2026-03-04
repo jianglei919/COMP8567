9aa1a2190939b4e6515e27e1f8251274bf0a5ecd
Lei Jiang <990864781@qq.com>
2026-03-03 19:15:57 -0500

    feat: -kgp function

--- A2/A2_Lei_Jiang.c
+++ A2/A2_Lei_Jiang.c
@@ -877,6 +877,58 @@ static void opt_sco(pid_t process_id)
     free_proc_list(&siblings);
 }
 
+/**
+ * -kgp：向 process_id 的父进程发送 SIGKILL 信号，要求它终止。要求不杀死 bash 进程（即使 bash 进程是 process_id 的父进程），以免影响用户的正常操作。
+ * 实现思路：
+ * 1. 先读取 process_id 的父进程 PPID，记为 parent_pid
+ * 2. 再读取 parent_pid 的父进程 PPID，记为 grandparent_pid
+ * 3. 如果 grandparent_pid 不存在或者不大于 1，就说明 parent_pid 没有父进程或者父进程是 init 进程了，这时不杀死 parent_pid
+ * 4. 如果 grandparent_pid 是 bash 进程，就不杀死 parent_pid
+ * 5. 否则，向 grandparent_pid 发送 SIGKILL 信号
+ */
+static void opt_kgp(pid_t process_id)
+{
+    pid_t parent_pid = -1;
+    if (read_ppid_from_proc(process_id, &parent_pid) != 0 || parent_pid <= 0)
+    {
+        fprintf(stderr, "Cannot determine parent process of %d\n", (int)process_id);
+        return;
+    }
+
+    pid_t grandparent_pid = -1;
+    if (read_ppid_from_proc(parent_pid, &grandparent_pid) != 0 || grandparent_pid <= 0)
+    {
+        fprintf(stderr, "Cannot determine grandparent process of %d\n", (int)process_id);
+        return;
+    }
+
+    if (grandparent_pid <= 1) // 为系统进程，不能杀死
+    {
+        fprintf(stderr, "Grandparent is not a user process and will not be terminated\n");
+        return;
+    }
+
+    if (is_bash_process(grandparent_pid))
+    {
+        fprintf(stderr, "Grandparent is BASH and will not be terminated\n");
+        return;
+    }
+
+    if (kill(grandparent_pid, SIGKILL) != 0)
+    {
+        if (errno == ESRCH)
+        {
+            fprintf(stderr, "Grandparent process %d no longer exists\n", (int)grandparent_pid);
+            return;
+        }
+
+        fprintf(stderr, "Failed to kill grandparent %d: %s\n", (int)grandparent_pid, strerror(errno));
+        return;
+    }
+
+    printf("SIGKILL was sent to process %d\n", (int)grandparent_pid);
+}
+
 int main(int argc, char **argv)
 {
 
@@ -964,8 +1016,8 @@ int main(int argc, char **argv)
         opt_sst(process_id);
     else if (strcmp(opt, "-sco") == 0)
         opt_sco(process_id);
-    // else if (strcmp(opt, "-kgp") == 0)
-    //     opt_kgp(process_id, &pv);
+    else if (strcmp(opt, "-kgp") == 0)
+        opt_kgp(process_id);
     // else if (strcmp(opt, "-kpp") == 0)
     //     opt_kpp(process_id, &pv);
     // else if (strcmp(opt, "-ksp") == 0)
